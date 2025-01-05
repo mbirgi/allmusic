3e68282d46e8e56686ec2bcb73ba91d97f58cd02
memo <mbirgi@gmail.com>
2025-01-05 14:52:55 +0100

    Integrate Spotify API to create and populate a playlist with extracted song data

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pprint
 from bs4 import BeautifulSoup as bs
 import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -14,10 +20,32 @@ with open(os.path.join(folder, filename)) as f:
 track_soup = soup.find('div', class_='descriptorSubGrid')
 tracks = []
 for row in track_soup.find_all('div', class_='songRow'):
-    # print(row.prettify())
-    # print('row type: ', type(row))
     track = {}
     track['title'] = row.find('span', class_='songTitle').get_text().strip()
     track['artist'] = row.find('div', class_='songRight').get_text().strip()
     tracks.append(track)
-print(tracks[0:3])
+
+# Spotify credentials
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'),
+    redirect_uri=os.getenv('SPOTIPY_REDIRECT_URI'),
+    scope='playlist-modify-public'
+))
+
+# Create a new playlist
+user_id = sp.current_user()['id']
+playlist = sp.user_playlist_create(user_id, f'{style} Allmusic', public=True)
+playlist_id = playlist['id']
+
+# Search for tracks and add them to the playlist
+for track in tracks:
+    query = f"{track['title']} {track['artist']}"
+    result = sp.search(query, type='track', limit=1)
+    if result['tracks']['items']:
+        track_id = result['tracks']['items'][0]['id']
+        sp.playlist_add_items(playlist_id, [track_id])
+
+print(f"Tracks added to playlist: {playlist['external_urls']['spotify']}")
+
+
