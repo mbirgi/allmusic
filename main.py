fca67ccc46ef9a145a6721e31146765de265da1f
memo <mbirgi@gmail.com>
2025-01-05 13:53:20 +0100

    add html parser

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ folder = 'data'
 filename = f'{style} Music Songs _ AllMusic.html'
 
 with open(os.path.join(folder, filename)) as f:
-    soup = bs(f)
+    soup = bs(f, features="html.parser")
 
-print(soup.prettify())
+track_soup = soup.find('div', class_='descriptorSubGrid')
+tracks = []
+for row in track_soup.find_all('div', class_='songRow'):
+    # print(row.prettify())
+    # print('row type: ', type(row))
+    track = {}
+    track['title'] = row.find('span', class_='songTitle').get_text().strip()
+    track['artist'] = row.find('div', class_='songRight').get_text().strip()
+    tracks.append(track)
+print(tracks[0:3])
